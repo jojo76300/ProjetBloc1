14834a9c77db9d8cfaf9129ce473e595e44f004c
Flomax7613 <maxence.allard.76@gmail.com>
2025-09-17 14:11:03 +0200

    Classe Championnat

---
+++ wwe/Models/Championnat.cs
@@ -0,0 +1,12 @@
+namespace wwe.Models;
+
+public class Championnat
+{
+    public string Nom { get; set; }
+    
+    public string Info { get; set; }
+    
+    public DateTime DateCreation { get; set; }
+    
+    public string Periode { get; set; }
+}
